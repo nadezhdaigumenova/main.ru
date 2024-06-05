8e5bbabd966c23d5937352a66a2ac95a6046e1c6
nadezhdaigumenova <nadya.kyzlakova@mail.ru>
2024-06-05 15:13:40 +0700

    Add files via upload

---
+++ main.ru.py
@@ -0,0 +1,22 @@
+a = 'cucumber'
+print(len(a))
+first = 4
+second = 3
+summa = first+second
+diff = first-second
+print(summa)
+print(diff)
+third = 2
+mean = (first+second+third)/3
+print(mean)
+first_string = 'Вторник'
+second_string = 'Понедельник'
+print(second_string,',' +first_string)
+a = 1
+b = 2
+c = 3
+f = (a*b)+(a*c)
+print(f**3/2)
+
+
+
